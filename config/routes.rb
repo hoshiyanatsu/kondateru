6556736127b57735a82ad55140061e4253cbcac5
星谷那月 <nya-tu@hoshikokunagatsunoMacBook-Pro.local>
2021-12-12 15:04:31 +0900

    コメント削除機能を追加、細かなcss

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :meals do
     resources :likes, only: [:create, :destroy]
-    resources :comments, only: [:create, ]
+    resource :comments, only: [:create]
     collection do
       get 'search'
     end
   end
+  resources :comments, only: [:destroy]
   root 'meals#index'
 end
