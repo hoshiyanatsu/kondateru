4e79079af00ff4fdb8281d90896794730a3e8ea0
星谷那月 <nya-tu@hoshikokunagatsunoMacBook-Pro.local>
2021-12-17 08:37:12 +0900

    css,htmlを変更、hoverをつけた

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'users/new' => 'users#new'
   get 'users/show' => 'users#show'
   get 'comments/create' => 'comments#create'
-  get 'meals/searchmain' => 'meals#searchmain'
   devise_for :users
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
